81c1cfc9abffa6fb76496d896c62de0205b766ab
Emilio <emilio.malacalza@gmail.com>
2021-06-13 14:44:53 -0300

    Load and setting of the background for the main game window

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # Módulos
-import sys, pygame
+import sys, os
+import pygame
 from pygame.locals import *
  
 # Constantes
@@ -13,16 +14,34 @@ HEIGHT = 480
  
 # Funciones
 # ---------------------------------------------------------------------
- 
+def load_image (filename, transparent=False):
+    try: image = pygame.image.load(filename)
+    except pygame.error:
+        raise SystemExit
+
+    image = image.convert_alpha() # more suitable for .png images
+
+    if transparent:
+        color = image.get_at((0,0))
+        image.set_colorkey(color, RLEACCEL)
+
+    return image
+
 # ---------------------------------------------------------------------
  
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Test")
+    background_image = load_image("images/fondo_pong.png")
+    
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit(0)
+        
+        screen.blit(background_image, (0, 0))
+        pygame.display.flip()
+
     return 0
  
 if __name__ == '__main__':
