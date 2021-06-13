b5233a58a54d71407adb44b15ef9d32b78d5f617
Emilio <emilio.malacalza@gmail.com>
2021-06-13 15:31:11 -0300

    Adds a clock that controls the movement of the ball

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-# Módulos
+# Modules
 import sys
 import pygame
 from pygame.locals import *
- 
-# Constantes
+
+# Constants
 WIDTH = 640
 HEIGHT = 480
- 
-# Clases
+FRAMERATE = 60
+X = 0
+Y = 1
+
+# Classes
 # ---------------------------------------------------------------------
 class Ball (pygame.sprite.Sprite):
     def __init__(self):
@@ -19,18 +22,30 @@ class Ball (pygame.sprite.Sprite):
         self.speed = [0.5, -0.5]
 
     def get_image (self): return self.image
-
     def get_rect (self): return self.rect
+
+    # updates the position of the ball on the screen
+    def update (self, time):
+        self.rect.centerx += self.speed[X] * time
+        self.rect.centery += self.speed[Y] * time
+
+        if (self.rect.left <= 0 or self.rect.right >= WIDTH):
+            self.speed[X] = -self.speed[X]  # changes of x momentum component
+            self.rect.centerx += self.speed[X] * time
+
+        if (self.rect.top <= 0 or self.rect.bottom >= HEIGHT):
+            self.speed[Y] = -self.speed[Y]  # changes of y momentum component
+            self.rect.centery += self.speed[Y] * time
 # ---------------------------------------------------------------------
- 
-# Funciones
+
+# Procedures
 # ---------------------------------------------------------------------
 def load_image (filename, transparent=False):
     try: image = pygame.image.load(filename)
     except pygame.error:
         raise SystemExit
 
-    image = image.convert_alpha() # more suitable for .png images
+    image = image.convert_alpha()   # more suitable for .png images
 
     if transparent:
         color = image.get_at((0,0))
@@ -39,27 +54,33 @@ def load_image (filename, transparent=False):
     return image
 
 # ---------------------------------------------------------------------
- 
+
 def main():
     # sets the main screen with its background image
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Test")
     background_image = load_image("images/fondo_pong.png")
-    
+
+    # creates a clock to control the game time
+    clock = pygame.time.Clock()
+
     # creates the pong ball
     ball = Ball()
 
     while True:
+        time = clock.tick(FRAMERATE)
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit(0)
-        
+
+        ball.update(time)   # updates the position of the ball
         screen.blit(background_image, (0, 0))
         screen.blit(ball.get_image(), ball.get_rect())
         pygame.display.flip()
 
     return 0
- 
+
 if __name__ == '__main__':
     pygame.init()
     main()
